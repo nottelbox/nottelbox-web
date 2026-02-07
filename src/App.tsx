c41f26666676aad490d008172c322fa8fbbdea1f
Simon Blum <simonblum@hotmail.de>
2026-02-07 10:46:45 +0100

    App routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { Routes, Route } from 'react-router'
 import './App.css'
 
 function App() {
 
   return (
-    <main style={{ padding: 40 }}>
-      <h1>🚧 Coming Soon</h1>
-      <p>Diese Website ist gerade im Aufbau.</p>
-    </main>
+    <Routes>
+      <Route path="/" element={<main style={{ padding: 40 }}>
+        <h1>🚧 Coming Soon</h1>
+        <p>Diese Website ist gerade im Aufbau.</p>
+      </main>}/>
+      <Route path="/vegan" element={<main style={{ padding: 40 }}>
+      <h1>Veganismus</h1>
+      <p>Das ethische Prinzip, dass Menschen ohne die Ausbeutung anderer Tiere leben sollen.</p>
+    </main>}/>
+    </Routes>
+    
   )
 }
 
